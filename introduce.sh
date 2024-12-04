14d93c9cb49ba9e943a3322b90e0e7da7333db86
Ubuntu <ubuntu@ip-172-31-82-253.ec2.internal>
2024-12-04 19:47:38 +0000

    fixed some mistakes

--- introduce.sh
+++ introduce.sh
@@ -2,8 +2,14 @@
 
 name=Kadyrbek
 age=20
-favourite_language=Kyrgyz
+favourite_language=Bash
 hobby=Soccer
-dream=becoming a billionaire
+dream="becoming a billionaire"
 
-echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time playing $hobby. As a $age-year-old, I believe it's never too late to follow your dreams. My favorite programming language is $favorite_language. I code in $favorite_language almost every day! One day, I hope my love for $favorite_language will help me achieve my dream of $dream. When I'm not busy working toward $dream, you'll find me playing $hobby to relax. That's a little about me, $name — a $age-year-old who loves $favorite_language, enjoys $hobby, and dreams of $dream!"
+echo "Hi, my name is $name, and I'm $age years old. Every morning, $name likes to spend time playing $hobby.
+As a $age-year-old, I believe it's never too late to follow your dreams.
+My favorite programming language is $favourite_language.
+I code in $favourite_language almost every day!
+One day, I hope my love for $favourite_language will help me achieve my dream of $dream.
+When I'm not busy working toward $dream, you'll find me playing $hobby to relax.
+That's a little about me, $name — a $age-year-old who loves $favourite_language, enjoys $hobby, and dreams of $dream!"
